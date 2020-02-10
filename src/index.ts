9a226888b46629a6175b1036c7e73a1b5e3d59e3
Kosaburo Kaneko <b1840943@gmail.com>
2020-02-10 23:31:27 +0900

    グループ分け機能実装!

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import uniq from 'lodash.uniq';
 import xor from 'lodash.xor';
 import logger from './logger';
 import './env';
+import { envOf } from './utils';
 
 const sendInvitation = async (): Promise<void> => {
   try {
     const lunchLauncher = new LunchLauncher();
     await lunchLauncher.sendInvitation();
-    logger.info('ランチの招待メッセージが送信されました');
+    logger.info('ランチの招待メッセージが送信されました。');
   } catch (err) {
     logger.error(err);
   }
@@ -28,13 +29,49 @@ const sendReminder = async (): Promise<void> => {
     const membersInChannel = (await lunchLauncher.listMembers()).members;
     const unreactedUserIds = xor(reactedUserIdsUniq, membersInChannel);
     await lunchLauncher.sendRemider(unreactedUserIds, latestMessageByBot.ts);
-    logger.info('ランチのリマインダーメッセージが送信されました');
+    logger.info('ランチのリマインダーメッセージが送信されました。');
   } catch (err) {
     logger.error(err);
   }
 };
 
+const sendGroupListInfo = async (): Promise<void> => {
+  try {
+    const lunchLauncher = new LunchLauncher();
+    const latestMessageByBot = await lunchLauncher.getLatestMessageByBot();
+    if (!latestMessageByBot.reactions) {
+      throw new Error('リアクションが存在しません。');
+    }
+
+    for (const reaction of latestMessageByBot.reactions) {
+      if (
+        reaction.name ===
+        lunchLauncher.convertEmojiCodeToName(envOf('ACCEPT_EMOJI'))
+      ) {
+        const users = reaction.users.filter(
+          user => user !== envOf('SLACK_BOT_ID')
+        );
+        lunchLauncher.sendGroupListInfo(users);
+      }
+    }
+    logger.info('本日のランチのグループ情報が送信されました。');
+  } catch (err) {
+    logger.error(err);
+  }
+};
+
+// const testUsers: string[] = [];
+// for (let i = 0; i < 12; i++) {
+//   testUsers.push(`user${i}`);
+// }
+
+// const testsendGroupListInfo = (): void => {
+//   const lunchLauncher = new LunchLauncher();
+//   lunchLauncher.sendGroupListInfo(testUsers);
+// };
+
 (async (): Promise<void> => {
   await sendInvitation();
-  // await sendReminder();
+  await sendReminder();
+  await sendGroupListInfo();
 })();
--- src/slack.ts
+++ src/slack.ts
@@ -64,7 +64,7 @@ export default class LunchLauncher {
     await this.addInitialReaction(result.ts as string);
   }
 
-  async addInitialReaction(ts: string): Promise<void> {
+  private async addInitialReaction(ts: string): Promise<void> {
     await this.web.reactions.add({
       channel: this.channelId,
       name: this.convertEmojiCodeToName(envOf('ACCEPT_EMOJI')),
@@ -129,12 +129,74 @@ export default class LunchLauncher {
     });
   }
 
-  async getPermaLink(messageTs: string): Promise<string> {
+  private async getPermaLink(messageTs: string): Promise<string> {
     const result = (await this.web.chat.getPermalink({
       channel: this.channelId,
       message_ts: messageTs
     })) as PermaLink;
     if (!result.ok) throw new Error('異常なレスポンスを検知しました。');
     return result.permalink;
+  }
+
+  private makeGroup(users: string[]): string[][] {
+    const totalPeopleNumber = users.length;
+    const maxPeoplePerGroup = Number(envOf('MAX_PEOPLE_PER_GROUP'));
+
+    if (totalPeopleNumber > maxPeoplePerGroup) {
+      let devider = 1;
+      const group: string[][] = [];
+
+      while (group.length === 0 || group[0].length > maxPeoplePerGroup) {
+        devider += 1;
+        group[0] = users.slice(0, Math.ceil(totalPeopleNumber / devider));
+      }
+
+      let startNumber = 0;
+      for (let i = 1; i < devider; i++) {
+        startNumber += group[i - 1].length;
+        group[i] = users.slice(
+          startNumber,
+          Math.floor(totalPeopleNumber / devider) + startNumber
+        );
+      }
+
+      return group;
+    }
+
+    return [users];
+  }
+
+  async sendGroupListInfo(users: string[]): Promise<void> {
+    const group = this.makeGroup(users);
+    if (group.length === 1) return;
+
+    const mentionableGroup = this.convertUserIdToMentionable(group);
+    const text = this.makeGroupListText(mentionableGroup);
+    const result = await this.web.chat.postMessage({
+      channel: this.channelId,
+      text
+    });
+    if (!result.ok) throw new Error('異常なレスポンスを検知しました。');
+  }
+
+  private makeGroupListText(groups: string[][]): string {
+    const preMsg = `今日はこのグループで${envOf(
+      'LUNCH_TIME'
+    )}からランチに行きます :hamburger: \nみなさん準備してください！\n`;
+    const msg = [preMsg];
+
+    for (const [i, group] of groups.entries()) {
+      msg.push(`${i + 1}つ目のグループは${group}です！`);
+    }
+
+    return msg.join('\n');
+  }
+
+  private convertUserIdToMentionable(userIds: string[][]): string[][] {
+    return userIds.map(userIds =>
+      userIds.map(userId => {
+        return `<@${userId}>`;
+      })
+    );
   }
 }
